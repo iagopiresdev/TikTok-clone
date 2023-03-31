82487a7ba3d47400f1857181a17e1a46ada115bd
Iago Pires <iagopires.dev@gmail.com>
2023-03-31 19:26:58 -0400

    Aula 3

--- src/assets/components/Video.tsx
+++ src/assets/components/Video.tsx
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from "react";
+import VideoFooter from "./VideoFooter";
 import "./video.css";
 
 function Video(){
@@ -24,7 +25,8 @@ function Video(){
                 loop
                 src="https://firebasestorage.googleapis.com/v0/b/jornada2-eb156.appspot.com/o/ZqU6oFX6.mp4.mp4?alt=media&token=9839e872-2d5e-4da3-9299-17eb2949831d"
                 //src="https://cdn.discordapp.com/attachments/764337208844222525/1089904310659338331/InShot_20230327_162807719.mp4"
-            ></video>
+                ></video>
+            <VideoFooter />
         </div>
     );
 }
---
+++ src/assets/components/VideoFooter.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./videoFooter.css";
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+
+function VideoFooter(){
+    return (
+        <div className="videoFooter">
+            <div className="videoFooter__text">
+                <h3>@username</h3>
+                <p>This is a description</p>
+                <div className="videoFooter__ticker">
+                    <MusicNoteIcon className="videoFooter__icon"/>
+                    <div className="videoFooterMusic__text">
+                        <p>Music title</p>
+                    </div>
+                </div>
+            </div>
+            <img className="videoFooter__record" src="https://static.thenounproject.com/png/934821-200.png" alt="" />
+        </div>
+    )
+}
+
+export default VideoFooter;

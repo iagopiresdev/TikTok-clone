a07fd06dab90f5abe976e1bcfa7f85f4fba9060e
Iago Pires <iagopires.dev@gmail.com>
2023-04-25 23:55:59 -0400

    preparing application for redux

--- src/assets/components/hooks/useVideos.tsx
+++ src/assets/components/hooks/useVideos.tsx
@@ -1,50 +1,19 @@
-import React, { useRef, useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { RootState } from "../../store/store";
-import { pauseVideo } from "../../store/video/videoActions";
-import VideoFooter from "./VideoFooter";
-import VideoSidebar from "./VideoSidebar";
-import { VideoProps } from './types/VideoProps';
-import "./video.css";
+import { useState, useEffect } from 'react';
+import db from "../../../config/firebase";
+import { collection, getDocs } from 'firebase/firestore/lite';
+import { VideoProps } from '../types/VideoProps';
 
-function VideoPlayer({
-  likes,
-  messages,
-  shares,
-  name,
-  description,
-  music,
-  url,
-}: VideoProps) {
-  const videoRef = useRef<HTMLVideoElement>(null);
-  const dispatch = useDispatch();
-  const isPlaying = useSelector((state: RootState) => state.video.isPlaying);
+export const useVideos = () => {
+  const [videos, setVideos] = useState<VideoProps[]>([]);
 
   useEffect(() => {
-        if (videoRef.current) {
-      videoRef.current.pause();
-      dispatch(pauseVideo());
-    }
-  }, [url, dispatch]);
-
-  function handleStart() {
-    if (isPlaying) {
-      videoRef.current?.pause();
-      dispatch(pauseVideo());
-    } else {
-      videoRef.current?.play();
-      dispatch(pauseVideo()); // Update this line to an appropriate action for playing video
-    }
-  }
-
-  return (
-    <div className="video">
-      <video className="video_player" ref={videoRef} onClick={handleStart} loop src={url} />
-      <VideoSidebar likes={likes} messages={messages} shares={shares} />
-      <VideoFooter {...{ name, description, music }} />
-    </div>
-  );
-}
-
-export default VideoPlayer;
-
+    const getVideos = async () => {
+      const videosCollection = collection(db, 'videos');
+      const videoSnapshot = await getDocs(videosCollection);
+      const videoList: VideoProps[] = videoSnapshot.docs.map(doc => doc.data() as VideoProps);
+      setVideos(videoList);
+    };
+    getVideos();
+  }, []);
+  return videos;
+};
